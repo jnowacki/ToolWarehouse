86de1f94a6a89e334fb855b46581d50a50393b04
Jedrzej Nowacki <jedrzej.nowacki@trimetis.com>
2019-05-19 14:51:45 +0200

    6th step: auth filter redirection

---
+++ src/main/java/pl/jnowacki/filter/AuthFilter.java
@@ -0,0 +1,25 @@
+package pl.jnowacki.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthFilter", servletNames = {"ToolList"})
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (req.getSession().getAttribute("userName") == null
+                && !req.getRequestURI().endsWith("/login")) {
+            res.sendRedirect("/login");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
