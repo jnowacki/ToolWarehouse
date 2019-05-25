c4048993adf2de0bdc392982a9b8636984de167b
Jedrzej Nowacki <jedrzej.nowacki@trimetis.com>
2019-05-25 11:58:27 +0200

    9th step: user service

--- src/main/java/pl/jnowacki/service/UserService.java
+++ src/main/java/pl/jnowacki/service/UserService.java
@@ -1,8 +1,16 @@
 package pl.jnowacki.service;
 
+import pl.jnowacki.dao.UserDao;
+import pl.jnowacki.dao.UserDaoImpl;
+
 public class UserService {
 
+    private UserDao userDao = new UserDaoImpl();
+
     public boolean isUserValid(String login, String password) {
-        return false;
+
+        return userDao.getUser(login)
+                .map(user -> password.equals(user.getPassword()) && login.equals(user.getLogin()))
+                .orElse(false);
     }
 }
